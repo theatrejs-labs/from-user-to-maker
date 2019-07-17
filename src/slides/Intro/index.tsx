9dd020f7d37724516f5984626c6e038aeb3d4ebd
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-17 10:15:58 +0430

    step 2 is done

--- src/slides/Intro/index.tsx
+++ src/slides/Intro/index.tsx
@@ -10,24 +10,29 @@ import title2 from './assets/Title-Part2.svg'
 export default class extends Slide {
 
     state = {
-        appear: false
+        appear: false,
+        showTable: false
     }
 
     steps = [
-        () => this.setState({ appear: true })
+        () => this.setState({ appear: true }),
+        () => this.setState({ showTable: true })
     ]
     backwardSteps = [
-        () => this.setState({ appear: false })
+        () => this.setState({ appear: false }),
+        () => this.setState({ showTable: false })
     ]
 
     get content () {
-        const { appear } = this.state
+        const { appear, showTable } = this.state
         return (
-            <div className={'Intro' + (appear ? ' appear' : '')}>
-                <div className="line left" />
-                <div className="line right" />
-                <img className="Intro__title Intro__title__part1" src={title} alt="Design for a" />
-                <img className="Intro__title Intro__title__part2" src={title2} alt="Dynamic Medium" />
+            <div className={'Intro' + (appear ? ' appear' : '') + (showTable ? ' showTable' : '')}>
+                <div className="point">
+                    <div className="line left" />
+                    <div className="line right" />
+                    <img className="Intro__title Intro__title__part1" src={title} alt="Design for a" />
+                    <img className="Intro__title Intro__title__part2" src={title2} alt="Dynamic Medium" />
+                </div>
             </div>
         )
     }
