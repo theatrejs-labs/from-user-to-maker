14a466a94ea907f071bb59466355fe9dafc68679
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-17 17:27:35 +0430

    feat: codebox added

--- src/slides/Inklings/components/Code/index.tsx
+++ src/slides/Inklings/components/Code/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import theme from './duotone-space';
 
 import './style.scss'
 
@@ -10,6 +12,9 @@ export default class Code extends React.Component<IProps, IState> {
     render () {
         return (<div className="Comparison__panel code-box">
             <h1>Code</h1>
+            <SyntaxHighlighter className="code-box__prism" language="javascript" style={theme}>
+                const a = 2 + 1;
+            </SyntaxHighlighter>
         </div>)
     }
 
