ff70baa49d50c2671f954e912e1a053a406d1a5a
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-17 09:18:13 +0430

    feat: very first slide

--- src/slides/Intro/index.tsx
+++ src/slides/Intro/index.tsx
@@ -3,25 +3,14 @@ import React from 'react'
 import './style.scss'
 
 import Slide from '../../shared/Slide';
+import title from './assets/TheDynamicDesignTool.svg'
 
 export default class extends Slide {
-    
-    steps = [
-        () => { console.log('a') },
-        () => { console.log('b') },
-        () => { console.log('c') },
-        () => { console.log('d') },
-    ]
-
-    backwardSteps = [
-        () => { console.log('ba') },
-        () => { console.log('bb') },
-        () => { console.log('bc') },
-        () => { console.log('bd') },
-    ]
 
     get content () {
-        return (<h1 style={{color: 'white'}}>{Math.random()}</h1>)
+        return (<div>
+            <img src={title} alt="The Dynamic Design Tool" />
+        </div>)
     }
 
 }
