f7bd666d613d69c1b35a63e9904990ba71c4f502
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-17 19:50:37 +0430

    s: PlaceHolders in use

--- src/slides/Inklings/index.tsx
+++ src/slides/Inklings/index.tsx
@@ -6,14 +6,21 @@ import Slide from '../../shared/Slide'
 
 import one from './assets/one.svg'
 import title from './assets/inklings.svg'
+import games from './assets/games.svg'
+import oldschool from './assets/oldschool.svg'
+
 import Comparison from './components/Comparison';
+import PlaceHolder from '../../shared/PlaceHolder';
 
 export default class extends Slide {
 
     state = {
-        appear: true,
+        appear: false,
+        slide1: 'progress',
         comparisonMode: false,
-        comparisonState: 0
+        comparisonState: 0,
+        slide2: 'progress',
+        slide3: 'progress'
     }
 
     steps = [
@@ -21,6 +28,9 @@ export default class extends Slide {
             const { changeBackgroundStyle } = this.props
             if (changeBackgroundStyle) changeBackgroundStyle({})
             this.setState({ appear: true })
+        },
+        () => {
+            this.setState({ slide1: 'current' })
         },
         () => {
             const { changeBackgroundStyle } = this.props
@@ -46,7 +56,7 @@ export default class extends Slide {
     ]
 
     get content () {
-        const { appear, comparisonMode, comparisonState } = this.state
+        const { appear, comparisonMode, comparisonState, slide1, slide2, slide3 } = this.state
         return (
             <div className={'Inklings' + (appear ? ' appear' : '') + (comparisonMode ? ' comparison-mode' : '')}>
                 <div className="point">
@@ -54,6 +64,7 @@ export default class extends Slide {
                     <img className="chapter" src={one} alt="Chapter One" />
                     <img className="Inklings__title" src={title} alt="Inklings" />
                 </div>
+                <PlaceHolder src={games} status={'progress'} />
                 <Comparison appear={comparisonMode} state={comparisonState} />
             </div>
         )
