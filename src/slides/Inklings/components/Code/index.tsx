f99b09787a3de4c67a37a63b47f9c65526c18ef1
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-17 19:04:48 +0430

    slides/Inklings/components/Code: improvements

--- src/slides/Inklings/components/Code/index.tsx
+++ src/slides/Inklings/components/Code/index.tsx
@@ -10,10 +10,14 @@ interface IState {}
 export default class Code extends React.Component<IProps, IState> {
 
     render () {
+        const code = `const circle = new Circle({ radius: 10 })`
         return (<div className="Comparison__panel code-box">
             <h1>Code</h1>
+            <div className="result">
+                <div className="circle"></div>
+            </div>
             <SyntaxHighlighter className="code-box__prism" language="javascript" style={theme}>
-                const a = 2 + 1;
+                {code}
             </SyntaxHighlighter>
         </div>)
     }
