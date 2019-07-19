60e6dbd05f96b701bca3b7d495fdb3b80b33a247
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-19 18:32:08 +0430

    build is on repo

---
+++ build/precache-manifest.d2192589c4f8bf61fe7e26292fbe6138.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc0efe087fadacc27d95c4ebe153d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2b7f05583edbd490f1b",
+    "url": "/static/css/main.cc5ef626.chunk.css"
+  },
+  {
+    "revision": "b8927c6799db3d456086",
+    "url": "/static/js/2.e829d598.chunk.js"
+  },
+  {
+    "revision": "e2b7f05583edbd490f1b",
+    "url": "/static/js/main.a79e16ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3410e874ad6fdf5e8f921d568b4d074e",
+    "url": "/static/media/HereBeDragons.3410e874.svg"
+  },
+  {
+    "revision": "4221fff72cea442638f9359c01d4c57e",
+    "url": "/static/media/TheDynamicDesignTool.4221fff7.svg"
+  },
+  {
+    "revision": "e583665b8ab077f97ba2b332e6fb5f34",
+    "url": "/static/media/Title-Part1.e583665b.svg"
+  },
+  {
+    "revision": "f0e3f36b03c866f440bcaae803ce9cdc",
+    "url": "/static/media/Title-Part2.f0e3f36b.svg"
+  },
+  {
+    "revision": "fe0d7f78254303b5d77444ec980fc17f",
+    "url": "/static/media/UsersAndMakers.fe0d7f78.svg"
+  },
+  {
+    "revision": "de92ce155d05fd377abd294bfd6bbf8a",
+    "url": "/static/media/background.de92ce15.svg"
+  },
+  {
+    "revision": "513bfa5914ada937115b3779836c1e0f",
+    "url": "/static/media/cursor.513bfa59.svg"
+  },
+  {
+    "revision": "908cd3893091e5aaaac6e475f60a7af9",
+    "url": "/static/media/emily1.908cd389.svg"
+  },
+  {
+    "revision": "e813b535690bc0645a2228c0337e14db",
+    "url": "/static/media/emily2.e813b535.svg"
+  },
+  {
+    "revision": "b3003e60877b7f554e8e6cae1ed93485",
+    "url": "/static/media/emily3.b3003e60.svg"
+  },
+  {
+    "revision": "89630f5d5bf9d4a7e82731fe675c8b67",
+    "url": "/static/media/games.89630f5d.svg"
+  },
+  {
+    "revision": "cc4dff177806c32157ac3bd33643c86f",
+    "url": "/static/media/infinity.cc4dff17.svg"
+  },
+  {
+    "revision": "83e5856e02efec370d93f975c312795f",
+    "url": "/static/media/inklings.83e5856e.svg"
+  },
+  {
+    "revision": "bb7e43f017a99ee978a1d6c4e28cfbf4",
+    "url": "/static/media/oldschool.bb7e43f0.svg"
+  },
+  {
+    "revision": "0178d633a8177c5da2fbcc5852b6eb34",
+    "url": "/static/media/one.0178d633.svg"
+  },
+  {
+    "revision": "f03f9b26f37c89a3f73c8f409a0adb32",
+    "url": "/static/media/row1.f03f9b26.svg"
+  },
+  {
+    "revision": "c5eaac21cba4eae886e9e6437e1dc78d",
+    "url": "/static/media/row2.c5eaac21.svg"
+  },
+  {
+    "revision": "7b16b4a4df5529a25ccb6984595afc39",
+    "url": "/static/media/row3.7b16b4a4.svg"
+  },
+  {
+    "revision": "f2f59ae555af20826a92cb1de00be0cd",
+    "url": "/static/media/step1.f2f59ae5.svg"
+  },
+  {
+    "revision": "646887fb643d0daf1c5107332bbd41cc",
+    "url": "/static/media/step2.646887fb.svg"
+  },
+  {
+    "revision": "cb8f4dc402d170866e09cb0982c352ba",
+    "url": "/static/media/step3.cb8f4dc4.svg"
+  },
+  {
+    "revision": "f438e2ed27b5d7db707ae27501ec1416",
+    "url": "/static/media/step4.f438e2ed.svg"
+  },
+  {
+    "revision": "2a6917ee247e8629412bd89d20cc6a84",
+    "url": "/static/media/three.2a6917ee.svg"
+  },
+  {
+    "revision": "298bca549ebe922071d7222f1a01dc67",
+    "url": "/static/media/two.298bca54.svg"
+  }
+]);
